b5f3ce7bbc0f8fd1adf4f095c093d7b0f1b42f44
MELIN Gregoire <gremelin@labegum.pedago.ipb.fr>
2017-12-13 19:04:18 +0100

    MGD ok

---
+++ mgd_2.m
@@ -0,0 +1,82 @@
+clear all;
+close all;
+clc;
+% ------------------------------------------------ %
+% ---- Instanciation des variables --------------- %
+% ------------------------------------------------ %
+syms x y t;
+
+
+
+%Coordonnees des points du triangle
+c2 = 15.91;
+c3 = 0;
+d3 = 10;
+T_points=[0 0;c2 0;c3 d3];
+
+%Parametres articulaires (mis au carre)
+rho1 = 14.98^2; 
+rho2 = 15.38^2;
+rho3 = 12^2;
+p_joint=[rho1 rho2 rho3];
+theta = 0.882603;
+
+%Longueurs des cotes de la plateforme
+l2 = 17.04;
+l3 = 20.84;
+l1 = l2^2+l3^2 - 2 * l2 * l3 * cos(theta); %Al kashi
+
+T_lengths=[l1 l2 l3];
+
+%Necessaire pour un changement de variable pour la resolution
+sinus_phi = (2*t)/(1+t^2); 
+cosinus_phi= (1-t^2)/(1+t^2);
+
+% ------------------------------------------------ %
+% ---- MISE EN PLACE DU MGD ---------------------- %
+% ------------------------------------------------ %
+
+%On exprime rho1, rho2 et rho3 en fonction des longueurs des plateformes
+%rho1^2=x^2 + y^2;  [1]
+%rho2^2=(x+l2*cos(phi)-c2)^2 + (y+l2*sin(phi))^2;   [2]
+%rho^3=(x+l3*cos(phi+beta)-c3)^2+(y+l3*sin(phi+beta)-d3)^2; [3]
+%En faisant [3] - [1] et [2] - [1] : on obtient le systeme suivant
+
+R = 2*l2*cosinus_phi-2*c2;
+S = 2*l2*sinus_phi;
+Q = -2*c2*l2*cosinus_phi + l2^2 + c2^2;
+U = 2*l3*(cosinus_phi*cos(theta)-sinus_phi*sin(theta))-2*c3;
+V = 2*l3*(sinus_phi*cos(theta)+cosinus_phi*sin(theta)) -2*d3;
+W = -2*d3*l3*(sinus_phi*cos(theta)+cosinus_phi*sin(theta)) - 2*c3*l3*(cosinus_phi*cos(theta)-sinus_phi*sin(theta)) + l3^2 + c3^2 + d3^2;
+
+eqn_xy = (S*(rho3 - rho1 - W) - V*(rho2 - rho1 - Q))^2 +(R*(rho3 - rho1 - W) - U*(rho2 - rho1 - Q))^2 -rho1*(R*V - S*U)^2;
+eqn_xy = simplify(eqn_xy);
+
+% ------------------------------------------------ %
+% ------------- RESOLUTION  ---------------------- %
+% ------------------------------------------------ %
+
+%On cherche a annuler le polynome d'ou le fait de n'etudier que son denominateur
+P = numden(eqn_xy);
+
+%Mise sous forme de polynome
+C = coeffs(P(1,1));
+C=fliplr(C); % Necessaire
+
+%Calcul des racines du polynome
+rotz = roots(C);
+
+%On a p_positione phi = 2*atan(t)*180/pi : Calcul des orientations p_positionsibles pour
+%des coordonnees articulaires donnees
+rotz_phi = atan(rotz)*2*180/3.14;
+
+for i=1:6
+  
+    p_position(i,1) = -(S*(rho3 - rho1 - W) - V*(rho2 - rho1 - Q))/(R*V - S*U);
+    p_position(i,2) =  (R*(rho3 - rho1 - W) - U*(rho2 - rho1 - Q))/(R*V - S*U);
+    p_position(i,1) = subs( p_position(i,1) ,t, rotz(i,1));
+    p_position(i,2) = subs( p_position(i,2) ,t, rotz(i,1));
+    p_position(i,3) = rotz(i,1); 
+    difference=rho1-(p_position(i,1)^2+p_position(i,2)^2); % Doit etre nulle si les p_positionitions sont justes
+    
+end
